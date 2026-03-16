0306b9190363d91cbc32e4c8b476d6495e9132fb
Glen Ford <anquiloglenford9@gmail.com>
2026-03-16 10:51:42 +0800

    Refactor Item class for private discount property
    Refactor Item class to update its definition.

--- Lab4(javascript_git)/solution.js
+++ Lab4(javascript_git)/solution.js
@@ -95,20 +95,6 @@ let players = [
 
 console.log(getTopScorers(players));
 
-// problem 5 The Private Inventory
-class Item {
-    #discount = 0.1;  
-    
-    constructor(name, price) {
-        this.name = name;
-        this.price = price;
-    }
-    
-    get finalPrice() {
-        let discountAmount = this.price * this.#discount;
-        return this.price - discountAmount;
-    }
-}
 
 // Problem 5: The Private Inventory
 class Item {
